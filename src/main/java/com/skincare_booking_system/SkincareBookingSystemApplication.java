5cb29bbaf0e5fcb0ae697feb26d455a01c424b48
DuyNguyen-3006 <155596767+DuyNguyen-3006@users.noreply.github.com>
2025-02-10 21:57:54 +0700

    first commit

--- src/main/java/com/skincare_booking_system/SkincareBookingSystemApplication.java
+++ src/main/java/com/skincare_booking_system/SkincareBookingSystemApplication.java
@@ -8,7 +8,6 @@ public class SkincareBookingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkincareBookingSystemApplication.class, args);
-		System.out.println("Hello World!");
 	}
 
 }
