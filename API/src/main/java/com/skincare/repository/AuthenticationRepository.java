687c96fc127c7548d4a5b77b750da2a8026ecb15
SkinCareBooking <kypase184884@fpt.edu.vn>
2025-02-11 14:41:10 +0700

    API register, viewprofile, delete

--- API/src/main/java/com/skincare/repository/AuthenticationRepository.java
+++ API/src/main/java/com/skincare/repository/AuthenticationRepository.java
@@ -4,8 +4,5 @@ import com.skincare.entity.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-
 public interface AuthenticationRepository  extends JpaRepository<User,Long>{
 }
