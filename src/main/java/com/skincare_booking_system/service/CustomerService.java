bd02540bab732655823b01150ce9f9a022d60119
DuyNguyen-3006 <155596767+DuyNguyen-3006@users.noreply.github.com>
2025-02-11 15:07:04 +0700

    fix code

--- src/main/java/com/skincare_booking_system/service/CustomerService.java
+++ src/main/java/com/skincare_booking_system/service/CustomerService.java
@@ -1,7 +1,5 @@
 package com.skincare_booking_system.service;
 
-import com.skincare_booking_system.dto.request.CustomerLoginRequest;
-import com.skincare_booking_system.entity.User;
 import com.skincare_booking_system.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
